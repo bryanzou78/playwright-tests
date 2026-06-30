4765154293d411fcf2e49495d42d63306ca1e142
Bryan Zou <175367072+bryanzou78@users.noreply.github.com>
2026-06-29 19:22:09 -0700

    add link navigation test

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -4,9 +4,14 @@ def test_homepage_loads(page):
     page.goto('https://www.wikipedia.org')
     expect(page).to_have_title('Wikipedia')
 
-def test_search_goes_to_correct_article(page):
+def test_search_navigates_to_correct_article(page):
     page.goto('https://www.wikipedia.org')
     page.locator('input#searchInput').click()
     page.locator('input#searchInput').fill('Albert Einstein')
     page.keyboard.press('Enter')
     expect(page.locator('h1')).to_contain_text('Albert Einstein')
+
+def test_clicking_link_navigates_to_article(page):
+    page.goto('https://en.wikipedia.org/wiki/Albert_Einstein')
+    page.locator("a[title='Theoretical physicist']").click()
+    expect(page.locator('#firstHeading')).to_contain_text('Theoretical physics')
