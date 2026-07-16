4d14f24a8b36864b6f6471ddff1a399bc7d0f949
Bryan Zou <175367072+bryanzou78@users.noreply.github.com>
2026-07-15 18:57:08 -0700

    add starting nav fixtures

--- conftest.py
+++ conftest.py
@@ -3,3 +3,13 @@ import pytest
 @pytest.fixture(scope="session")
 def browser_type_launch_args(browser_type_launch_args):
     return {**browser_type_launch_args, "slow_mo": 1000}
+
+@pytest.fixture
+def page_on_wiki_homepage(page):
+    page.goto('https://www.wikipedia.org')
+    yield page
+
+@pytest.fixture
+def page_on_einstein_article(page):
+    page.goto('https://en.wikipedia.org/wiki/Albert_Einstein')
+    yield page
--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,21 +1,19 @@
 from playwright.sync_api import expect
 
-def test_homepage_loads(page):
-    page.goto('https://www.wikipedia.org')
+def test_homepage_loads(page,page_on_wiki_homepage):
     expect(page).to_have_title('Wikipedia')
 
-def test_search_navigates_to_correct_article(page):
-    page.goto('https://www.wikipedia.org')
+
+def test_search_navigates_to_correct_article(page, page_on_wiki_homepage):
     page.locator('input#searchInput').click()
     page.locator('input#searchInput').fill('Albert Einstein')
     page.keyboard.press('Enter')
     expect(page.locator('h1')).to_contain_text('Albert Einstein')
 
-def test_clicking_link_navigates_to_article(page):
-    page.goto('https://en.wikipedia.org/wiki/Albert_Einstein')
+def test_clicking_link_navigates_to_article(page, page_on_einstein_article):
     page.locator("a[title='Theoretical physicist']").click()
     expect(page.locator('#firstHeading')).to_contain_text('Theoretical physics')
 
-def test_article_has_expected_section(page):
-    page.goto('https://en.wikipedia.org/wiki/Albert_Einstein')
+def test_article_has_expected_section(page, page_on_einstein_article):
     expect(page.locator('#Life_and_career')).to_be_visible()
+
